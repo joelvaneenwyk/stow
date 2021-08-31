2efd1c300ff8195c9e60924036c267726112842d
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-08-31 03:21:46 +0000

    Calculate the perl5 library manually

--- .github/make-stow.sh
+++ .github/make-stow.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-autoreconf -iv
+autoreconf --install --verbose
 eval "$(perl -V:siteprefix)"
 
 if [ -x "$(command -v cygpath)" ]; then
     siteprefix=$(cygpath "$siteprefix")
 fi
 
+PERL5LIB=$(perl -le 'print $INC[0]')
+export PERL5LIB
+
 echo "Site prefix: ${siteprefix:-NULL}"
-./configure --prefix="${siteprefix:-}" && make
+echo "Perl lib: $PERL5LIB"
+
+./configure --prefix="${siteprefix:-}" --with-pmdir="$PERL5LIB"
+make
+sudo make install
