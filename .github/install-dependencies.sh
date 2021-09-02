6fa5e3c15900e9cf4cb7f55b66d4a757451f3bb7
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-09-01 20:48:35 -0700

    Add automated config setup to Linux

--- .github/install-dependencies.sh
+++ .github/install-dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
+
 (
     echo "yes"
     echo ""
