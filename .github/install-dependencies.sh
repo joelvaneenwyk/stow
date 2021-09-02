557cf6c6c2c5e5f153e054dbbfa7436eea742b99
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-09-01 22:32:23 -0700

    Only use sudo if it exists

--- .github/install-dependencies.sh
+++ .github/install-dependencies.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sudo perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
-sudo cpan -i -T YAML Test::Output CPAN::DistnameInfo Module::Build
+if [ -x "$(command -v sudo)" ]; then
+    sudo perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
+    sudo cpan -i -T YAML Test::Output CPAN::DistnameInfo Module::Build
+else
+    perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
+    cpan -i -T YAML Test::Output CPAN::DistnameInfo Module::Build
+fi
