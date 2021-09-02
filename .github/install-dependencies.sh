373211cce7394661183c3772e76818bdbc3e6c68
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-09-01 21:17:19 -0700

    Missed files in previous submit

--- .github/install-dependencies.sh
+++ .github/install-dependencies.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
-
-(
-    echo "yes"
-    echo ""
-    echo "no"
-    echo "exit"
-) | cpan
-cpan -i -T YAML Test::Output CPAN::DistnameInfo Module::Build
+sudo perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
+sudo cpan -i -T YAML Test::Output CPAN::DistnameInfo Module::Build
