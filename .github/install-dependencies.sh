441cf77a44d7d172fd35fe51bdd87f8b4b5214c1
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-09-03 14:41:20 -0400

    Fix manifest and include functions on non-Windows platofrms as well

--- .github/install-dependencies.sh
+++ .github/install-dependencies.sh
@@ -8,5 +8,10 @@ function _sudo {
     fi
 }
 
+if [ -x "$(command -v apt-get)" ]; then
+    _sudo apt-get update
+    _sudo apt-get -y install texlive texinfo cpanminus
+fi
+
 _sudo perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
 _sudo cpan -i -T YAML Test::Output Test::More Test::Exception CPAN::DistnameInfo Module::Build Parse::RecDescent
