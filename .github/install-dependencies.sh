9dcf9ddd6ebc81900cb4894ab809a19c7d80b336
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-09-04 14:25:08 -0700

    Install cpanminus on msys2

--- .github/install-dependencies.sh
+++ .github/install-dependencies.sh
@@ -14,8 +14,24 @@ if [ -x "$(command -v apt-get)" ]; then
         texlive texinfo cpanminus \
         autoconf bzip2 \
         gawk curl libssl-dev make patch
+elif [ -x "$(command -v pacman)" ]; then
+    pacman -S --quiet --noconfirm --needed \
+        base-devel \
+        msys2-devel \
+        msys2-runtime-devel \
+        msys2-keyring \
+        openssl \
+        git \
+        gcc \
+        make \
+        autoconf \
+        automake1.16 \
+        automake-wrapper \
+        libtool \
+        libcrypt-devel \
+        perl
 fi
 
 _sudo perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
-_sudo cpan -i -T YAML Test::Output Test::More Test::Exception CPAN::DistnameInfo Module::Build Parse::RecDescent Inline::C
+_sudo cpan -i -T App::cpanminus YAML Test::Output Test::More Test::Exception CPAN::DistnameInfo Module::Build Parse::RecDescent Inline::C
 _sudo cpanm -n Devel::Cover::Report::Coveralls
