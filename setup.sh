55370480a3c935b681cad37706f4825daaf821cc
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-09-10 16:11:19 -0700

    Initial work to get docs building on Windows

--- setup.sh
+++ setup.sh
@@ -6,7 +6,15 @@
 autoreconf -iv
 
 # Set the 'siteprefix' variable based on configuration settings of perl
+siteprefix=
 eval "$(perl -V:siteprefix)"
+echo "Site prefix (default): $siteprefix"
+
+# Convert to unix path if on Cygwin/MSYS
+if [ -x "$(command -v cygpath)" ]; then
+    siteprefix=$(cygpath "$siteprefix")
+    echo "Site prefix    (unix): $siteprefix"
+fi
 
 # Run configure to generate 'Makefile' and then run make to create the
 # stow library and binary files e.g., 'stow', 'chkstow', etc.
--- tools/make-docs.bat
+++ tools/make-docs.bat
@@ -8,4 +8,14 @@ exit /b
 
     set _root=%~dp1
     set STOW_ROOT=%_root:~0,-1%
+
+    call "%~dp0install-texlive.bat"
+
+    set _perl_inc=-I tools\texinfo\tp\ -I tools\texinfo\tp\Texinfo\ -I tools\texinfo\tp\maintain\lib\Unicode-EastAsianWidth\lib\ -I tools\texinfo\tp\maintain\lib\libintl-perl\lib -I tools\texinfo\tp\maintain\lib\Text-Unidecode\lib\
+
+    perl %_perl_inc% "%STOW_ROOT%\tools\texinfo\tp\texi2any.pl" -I doc\ -o doc\ doc\stow.texi
+    echo @set UPDATED 0 0 0 >"%STOW_ROOT%\doc\version.texi"
+    echo @set UPDATED-MONTH ${2:-0} ${3:-0} >"%STOW_ROOT%\doc\version.texi"
+    echo @set EDITION $VERSION >"%STOW_ROOT%\doc\version.texi"
+    echo @set VERSION $VERSION >"%STOW_ROOT%\doc\version.texi"
 exit /b 0
