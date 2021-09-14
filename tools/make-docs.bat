7d8545252e6e582d7e271930712e347bd958b3e7
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-09-14 13:33:30 -0700

    Tweak doc generation script (not yet working)

--- tools/make-docs.bat
+++ tools/make-docs.bat
@@ -11,11 +11,13 @@ exit /b
 
     call "%~dp0install-texlive.bat"
 
-    set _perl_inc=-I tools\texinfo\tp\ -I tools\texinfo\tp\Texinfo\ -I tools\texinfo\tp\maintain\lib\Unicode-EastAsianWidth\lib\ -I tools\texinfo\tp\maintain\lib\libintl-perl\lib -I tools\texinfo\tp\maintain\lib\Text-Unidecode\lib\
-
-    perl %_perl_inc% "%STOW_ROOT%\tools\texinfo\tp\texi2any.pl" -I doc\ -o doc\ doc\stow.texi
     echo @set UPDATED 0 0 0 >"%STOW_ROOT%\doc\version.texi"
     echo @set UPDATED-MONTH ${2:-0} ${3:-0} >"%STOW_ROOT%\doc\version.texi"
     echo @set EDITION $VERSION >"%STOW_ROOT%\doc\version.texi"
     echo @set VERSION $VERSION >"%STOW_ROOT%\doc\version.texi"
+
+    set _perl_inc=-I tools\texinfo\tp\ -I tools\texinfo\tp\Texinfo\ -I tools\texinfo\tp\maintain\lib\Unicode-EastAsianWidth\lib\ -I tools\texinfo\tp\maintain\lib\libintl-perl\lib -I tools\texinfo\tp\maintain\lib\Text-Unidecode\lib\
+    set _cmd=perl %_perl_inc% "%STOW_ROOT%\tools\texinfo\tp\texi2any.pl" -I doc\ -o doc\ doc\stow.texi
+    echo ##[cmd] %_cmd%
+    %_cmd%
 exit /b 0
