11e84c674f1f09c1f44fbe55d28fb391cc9445bb
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-09-14 23:59:21 -0400

    Fix minimal stow build

--- tools/make-stow-minimal.sh
+++ tools/make-stow-minimal.sh
@@ -23,16 +23,6 @@ function edit() {
 function make_stow() {
     cd "$STOW_ROOT" || true
 
-    VERSION=2.3.2
-    PERL=$(which perl)
-
-    PMDIR=${prefix:-}/share/perl5/site_perl
-
-    if ! PERL5LIB=$($PERL -V | awk '/@INC/ {p=1; next} (p==1) {print $1}' | grep "$PMDIR" | head -n 1); then
-        echo "ERROR: Failed to check installed Perl libraries."
-        PERL5LIB="$PMDIR"
-    fi
-
     echo "# Perl modules will be installed to $PMDIR"
     echo "#"
     if [ -n "$PERL5LIB" ]; then
@@ -54,50 +44,65 @@ function make_stow() {
     edit "$STOW_ROOT/lib/Stow.pm"
     edit "$STOW_ROOT/lib/Stow/Util.pm"
     echo "Created Stow libraries."
+
+    perl -I "$STOW_ROOT/lib" -I "$STOW_ROOT/bin" "$STOW_ROOT/bin/stow" --version
 }
 
-if [ -x "$(command -v apt-get)" ]; then
-    _sudo apt-get update
-    _sudo apt-get -y install \
-        perl bzip2 gawk curl libssl-dev make patch
-elif [ -x "$(command -v apk)" ]; then
-    if [ ! -x "$(command -v sudo)" ]; then
-        apk update
-        apk add sudo
-    else
-        sudo apk update
+function _install_dependencies() {
+    if [ -x "$(command -v apt-get)" ]; then
+        _sudo apt-get update
+        _sudo apt-get -y install \
+            perl bzip2 gawk curl libssl-dev make patch
+    elif [ -x "$(command -v apk)" ]; then
+        if [ ! -x "$(command -v sudo)" ]; then
+            apk update
+            apk add sudo
+        else
+            sudo apk update
+        fi
+
+        sudo apk add \
+            wget curl unzip xclip \
+            build-base gcc g++ make musl-dev openssl-dev zlib-dev \
+            perl perl-dev perl-utils \
+            bash openssl
+    elif [ -x "$(command -v pacman)" ]; then
+        pacman -S --quiet --noconfirm --needed \
+            msys2-keyring \
+            base-devel libtool libcrypt-devel openssl \
+            mingw-w64-x86_64-make mingw-w64-x86_64-gcc mingw-w64-x86_64-binutils \
+            mingw-w64-x86_64-perl \
+            mingw-w64-x86_64-poppler
+    fi
+}
+
+function _setup_perl() {
+    VERSION=2.3.2
+    PERL=$(which perl)
+    PMDIR=${prefix:-}/share/perl5/site_perl
+
+    if ! PERL5LIB=$($PERL -V | awk '/@INC/ {p=1; next} (p==1) {print $1}' | grep "$PMDIR" | head -n 1); then
+        echo "ERROR: Failed to check installed Perl libraries."
+        PERL5LIB="$PMDIR"
     fi
 
-    sudo apk add \
-        wget curl unzip xclip \
-        build-base gcc g++ make musl-dev openssl-dev zlib-dev \
-        perl perl-dev perl-utils \
-        bash openssl
-elif [ -x "$(command -v pacman)" ]; then
-    pacman -S --quiet --noconfirm --needed \
-        msys2-keyring \
-        base-devel libtool libcrypt-devel openssl \
-        mingw-w64-x86_64-make mingw-w64-x86_64-gcc mingw-w64-x86_64-binutils \
-        mingw-w64-x86_64-perl \
-        mingw-w64-x86_64-poppler
-fi
+    "$STOW_ROOT/tools/initialize-cpan-config.pl"
 
-"$STOW_ROOT/tools/initialize-cpan-config.pl"
-
-if [ ! -x "$(command -v cpanm)" ]; then
-    if [ -x "$(command -v curl)" ]; then
-        curl -L https://cpanmin.us | perl - App::cpanminus
-    else
-        _sudo cpan -i -T App::cpanminus
+    if [ ! -x "$(command -v cpanm)" ]; then
+        if [ -x "$(command -v curl)" ]; then
+            curl -L https://cpanmin.us | perl - --sudo App::cpanminus
+        else
+            _sudo cpan -i -T App::cpanminus
+        fi
     fi
-fi
 
-if [ -x "$(command -v pacman)" ]; then
-    cpanm --notest Carp Inline::C IO::File IO::Scalar Win32::Mutex
-else
-    cpanm --notest Carp Inline::C IO::File IO::Scalar
-fi
+    if [ -x "$(command -v pacman)" ]; then
+        _sudo cpanm --notest Carp Inline::C IO::File IO::Scalar Win32::Mutex
+    else
+        _sudo cpanm --notest Carp Inline::C IO::File IO::Scalar
+    fi
+}
 
+_install_dependencies
+_setup_perl
 make_stow
-
-perl -I "$STOW_ROOT/lib" -I "$STOW_ROOT/bin" "$STOW_ROOT/bin/stow" --version
