7f77da1b4a363d9a620c9fd797d0d669425a1b12
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-09-26 12:49:30 -0700

    Add clean step before setup on Windows

--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
 @echo off
 
+call "%~dp0tools\make-clean.bat"
 call "%~dp0tools\install-dependencies.bat"
 call "%~dp0tools\make-stow.bat"
--- tools/make-clean.bat
+++ tools/make-clean.bat
@@ -39,6 +39,7 @@ exit /b
     rmdir /q /s "%STOW_ROOT%\_build\" > nul 2>&1
     rmdir /q /s "%STOW_ROOT%\_Inline\" > nul 2>&1
     rmdir /q /s "%STOW_ROOT%\bin\_Inline\" > nul 2>&1
+    rmdir /q /s "%STOW_ROOT%\tools\_Inline\" > nul 2>&1
     rmdir /q /s "%STOW_ROOT%\_test\" > nul 2>&1
     rmdir /q /s "%STOW_ROOT%\autom4te.cache\" > nul 2>&1
     rmdir /q /s "%STOW_ROOT%\blib\" > nul 2>&1
@@ -46,4 +47,5 @@ exit /b
     rmdir /q /s "%STOW_ROOT%\stow\" > nul 2>&1
     rmdir /q /s "%STOW_ROOT%\cover_db\" > nul 2>&1
     rmdir /q /s "%STOW_ROOT%\tmp-testing-trees\" > nul 2>&1
+    rmdir /q /s "%STOW_ROOT%\stow-*\" > nul 2>&1
 exit /b 0
