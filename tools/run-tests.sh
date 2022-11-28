1efb28f3263fa4ce95a55ff10c2ce3cfd310c0a9
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-11-27 18:54:47 -0800

    Attempt at fixing GitHub action syntax

--- tools/run-tests.sh
+++ tools/run-tests.sh
@@ -16,6 +16,155 @@
 # along with this program. If not, see https://www.gnu.org/licenses/.
 #
 
+function __print_stack() {
+    if [ -n "${BASH:-}" ]; then
+
+        source_index=0
+        function_index=1
+        callstack_end=${#FUNCNAME[@]}
+
+        local callstack=""
+        while ((function_index < callstack_end)); do
+            function=${FUNCNAME[$function_index]+"${FUNCNAME[$function_index]}"}
+            callstack+=$(printf '\\n    >  %s:%d: %s()' "${BASH_SOURCE[$source_index]}" "${BASH_LINENO[$source_index]}" "${function:-}")
+            ((++function_index))
+            ((++source_index))
+        done
+
+        printf "%b\n" "$callstack" >&2
+    fi
+
+    return 0
+}
+
+function __safe_exit() {
+    _value=$(expr "${1:-}" : '[^0-9]*\([0-9]*\)' 2>/dev/null || :)
+
+    if [ -z "${_value:-}" ]; then
+        # Not a supported return value so provide a default
+        exit 199
+    fi
+
+    # We intentionally do not double quote this because we are expecting
+    # this to be a number and exit does not accept strings.
+    # shellcheck disable=SC2086
+    exit $_value
+}
+
+function __trap_error() {
+    _retval=$?
+
+    if [ ! "${STOW_DISABLE_TRAP:-}" == "1" ]; then
+        _line=${_stow_dbg_last_line:-}
+
+        if [ "${_line:-}" = "" ]; then
+            _line="${1:-}"
+        fi
+
+        if [ "${_line:-}" = "" ]; then
+            _line="[undefined]"
+        fi
+
+        # First argument is always the line number even if unused
+        shift
+
+        echo "--------------------------------------" >&2
+
+        if [ "${STOW_DEBUG_TRAP_ENABLED:-}" = "1" ]; then
+            echo "Error on line #$_line:" >&2
+        fi
+
+        # This only exists in a few shells e.g. bash
+        # shellcheck disable=SC2039,SC3044
+        if _caller="$(caller 2>&1)"; then
+            printf " - Caller: '%s'\n" "${_caller:-UNKNOWN}" >&2
+        fi
+
+        printf " - Code: '%s'\n" "${_retval:-}" >&2
+        printf " - Callstack:" >&2
+        __print_stack "$@" >&2
+    fi
+
+    # We always exit immediately on error
+    __safe_exit ${_retval:-1}
+}
+
+function __set_debug_trap() {
+    shopt -s extdebug
+
+    # aka. set -T
+    set -o functrace
+
+    _stow_dbg_line=
+    export _stow_dbg_line
+
+    _stow_dbg_last_line=
+    export _stow_dbg_last_line
+
+    # 'ERR' is undefined in POSIX. We also use a somewhat strange looking expansion here
+    # for 'BASH_LINENO' to ensure it works if BASH_LINENO is not set. There is a 'gist' of
+    # at https://bit.ly/3cuHidf along with more details available at https://bit.ly/2AE2mAC.
+    trap '__trap_error "$LINENO" ${BASH_LINENO[@]+"${BASH_LINENO[@]}"}' ERR
+
+    _enable_trace=0
+    _bash_debug=0
+
+    # Using debug output is performance intensive as the trap is executed for every single
+    # call so only enable it if specifically requested.
+    if [ "${STOW_ARG_DEBUG:-0}" = "1" ] && [ -z "${BATS_TEST_NAME:-}" ]; then
+        # Redirect only supported in Bash versions after 4.1
+        if [ "$BASH_VERSION_MAJOR" -eq 4 ] && [ "$BASH_VERSION_MINOR" -ge 1 ]; then
+            _enable_trace=1
+        elif [ "$BASH_VERSION_MAJOR" -gt 4 ]; then
+            _enable_trace=1
+        fi
+
+        if [ "$_enable_trace" = "1" ]; then
+            trap '[[ "${FUNCNAME:-}" == "__trap_error" ]] || {
+                    _stow_dbg_last_line=${_stow_dbg_line:-};
+                    _stow_dbg_line=${LINENO:-};
+                }' DEBUG || true
+
+            _bash_debug=1
+
+            # Error tracing (sub shell errors) only work properly in version >=4.0 so
+            # we enable here as well. Otherwise errors in subshells can result in ERR
+            # trap being called e.g. _my_result="$(errorfunc test)"
+            set -o errtrace
+
+            # If set, command substitution inherits the value of the errexit option, instead of unsetting it in the
+            # subshell environment. This option is enabled when POSIX mode is enabled.
+            shopt -s inherit_errexit
+
+            export STOW_DEBUG_TRAP_ENABLED=1
+        fi
+    fi
+
+    STOW_DEBUG_TRACE_FILE=""
+
+    # Output trace to file if that is supported
+    if [ "$_bash_debug" = "1" ] && [ "$_enable_trace" = "1" ]; then
+        STOW_DEBUG_TRACE_FILE="$STOW_HOME/.logs/init.xtrace.$(date +%s).log"
+        mkdir -p "$STOW_HOME/.logs"
+
+        # Find a free file descriptor
+        log_descriptor=${BASH_XTRACEFD:-19}
+
+        while ((log_descriptor < 31)); do
+            if eval "command >&$log_descriptor" >/dev/null 2>&1; then
+                eval "exec $log_descriptor>$STOW_DEBUG_TRACE_FILE"
+                export BASH_XTRACEFD=$log_descriptor
+                set -o xtrace
+                break
+            fi
+
+            ((++log_descriptor))
+        done
+    fi
+
+    export STOW_DEBUG_TRACE_FILE
+}
+
 function run_command_output_file() {
     output_file=$1
     shift
@@ -113,7 +262,12 @@ function test_perl_version() {
 
     # Install Perl dependencies on this particular version of Perl in case
     # that has not been done yet.
-    install_perl_dependencies
+    if install_perl_dependencies; then
+        echo "Installed dependencies."
+    else
+        return $?
+        echo "Failed to install dependencies."
+    fi
 
     _perl_test_args=(-I "$STOW_PERL_LOCAL_LIB/lib/perl5")
 
@@ -258,6 +412,8 @@ function run_stow_tests() {
     shopt -s inherit_errexit nullglob >/dev/null 2>&1 || true
     trap exit_handler EXIT
 
+    __set_debug_trap
+
     _test_argument="${1:-}"
 
     LIST_PERL_VERSIONS=0
@@ -279,10 +435,14 @@ function run_stow_tests() {
     fi
 
     if [ ! "$LIST_PERL_VERSIONS" == "1" ] && [ ! "$_test_argument" == "--no-install" ]; then
-        if ! install_system_dependencies; then
+        if install_system_dependencies; then
+            echo "Finished installation of system dependencies."
+        else
             echo "Failed to install dependencies."
             return 4
         fi
+    else
+        echo "Skipped install of system dependencies."
     fi
 
     if [ "$LIST_PERL_VERSIONS" == "1" ]; then
