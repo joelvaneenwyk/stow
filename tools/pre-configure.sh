14503b8d228e2df06bf243ea8d49fe355b77c83a
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-14 19:42:22 -0700

    Add error checking to scripts

--- tools/pre-configure.sh
+++ tools/pre-configure.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-autoreconf --install
+set -eax
+
+siteprefix=""
 eval "$(perl -V:siteprefix)"
+
+if [ ! -d "$siteprefix" ]; then
+    echo "siteprefix not found"
+    exit 1
+fi
+
+echo "Perl 'siteprefix': $siteprefix"
+autoreconf --install
+cpanm --notest --sudo Devel::Cover::Report::Coveralls Test::Output IO::Scalar
